57a987422f33ac95c8c6686e0ac5f99b3a5fa52c
Muhammed Marwan <shoukiabdlkalam@gmail.com>
2026-01-01 15:12:26 +0530

    login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,9 @@
 
 const users = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+
+
+
 //register
 exports.registerController = async (req,res)=>{
     console.log("inside register controller");
@@ -24,6 +28,44 @@ exports.registerController = async (req,res)=>{
    // res.status(200).json("request recieved")
     
 }
+
+
 //login
+exports.loginController = async(req,res)=>{
+    console.log("inside login controller");
+    const {email,password}=req.body
+    try{
+        
+        const existingUser = await users.findOne({email})
+        
+        if(existingUser){
+            
+            if(password == existingUser.password){
+                
+                const tocken = jwt.sign({userMail:existingUser.email, role:existingUser.role},process.env.JWTSECRET)
+                res.status(200).json({user:existingUser,tocken})
+
+            }else{
+                
+                res.status(401).json("incorrect Password/ Email !!!")
+            }
+
+        }else{
+            
+            res.status(404).json("Account Does not exist")
+        }
+        
+
+    }catch(error){
+    
+        console.log(error);
+        res.status(500).json(error)
+    
+    }
+    
+}
+
+
+
 //user profile edit
 //admin profile edit
--- routes/router.js
+++ routes/router.js
@@ -2,5 +2,11 @@ const express = require('express')
 const userController = require('../controllers/userController')
 const router = new express.Router()
 
+
+
 router.post('/register',userController.registerController)
+
+router.post('/login',userController.loginController)
+
+
 module.exports = router
